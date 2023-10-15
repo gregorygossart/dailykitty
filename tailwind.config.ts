9e197772ab92f5cc833319004ee4749e9568b7bb
Grégory Gossart <gregory.gossart@decathlon.com>
2023-10-16 00:20:24 +0200

    add NextUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react"; // NextUI
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}", // Next
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", // Next
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // Next
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}", // NextUI
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))", // Tailwind
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))", // Tailwind
       },
     },
   },
-  plugins: [],
-}
-export default config
+  darkMode: "class", // NextUI
+  plugins: [nextui()], // NextUI
+};
+export default config;
